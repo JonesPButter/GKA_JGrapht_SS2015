336118821b140b84ecabaf2b19b24af59f7e4518
JonesPButter <Jonas.j@hotmail.de>
2015-06-08 12:37:15 +0200

    AdvancedPrim in arbeit.. (FIBONACCI-HEAP)

--- GKA_Version_3_5/src/werkzeuge/algorithmen/prim/AdvancedPrimImpl.java
+++ GKA_Version_3_5/src/werkzeuge/algorithmen/prim/AdvancedPrimImpl.java
@@ -1,23 +1,215 @@
 package werkzeuge.algorithmen.prim;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import materialien.MyWeightedEdge;
 import materialien.Vertex;
 
 import org.jgrapht.Graph;
+import org.jgrapht.graph.WeightedPseudograph;
+import org.jgrapht.util.FibonacciHeap;
+import org.jgrapht.util.FibonacciHeapNode;
 
 public class AdvancedPrimImpl
 {
 
-    Graph<Vertex,MyWeightedEdge> _graph;
+    Graph<Vertex,MyWeightedEdge> _eingabeGraph;
+    Graph<Vertex,MyWeightedEdge> _simplePrimGraph;
+
+    FibonacciHeap<Vertex> _fibHeap;
+    Map<Vertex,FibonacciHeapNode<Vertex>> _fibInhalt;
+    List<Vertex> _simplePrimGraphVertices;
+    Map<Vertex,Double> _schlüssel;
+    long startTime;
+    long endTime;
+    int _kantenAnzahl;
+    private int _graphAccesses;
     
     public AdvancedPrimImpl(Graph<Vertex, MyWeightedEdge> graph)
     {
-        _graph = graph;
+        assert graph.vertexSet().size() > 0 : "Vorbedingung verletzt: graph.vertexSet().size() > 0";
+        assert graph.edgeSet().size() > 0 : "Vorbedingung verletzt: graph.edgeSet().size() > 0";
+        
+        _eingabeGraph = graph;
+        _simplePrimGraph = new WeightedPseudograph<>(MyWeightedEdge.class);
+        _simplePrimGraphVertices = new ArrayList<Vertex>(_eingabeGraph.vertexSet());
+        _schlüssel = new HashMap<>();
+        _fibInhalt = new HashMap<>();
+        _fibHeap = new FibonacciHeap<>();
+
+        _kantenAnzahl=0;
+        startAlgorithm();
+    }
+    
+    private void startAlgorithm()
+    {
+        Vertex start = _eingabeGraph.vertexSet().iterator().next();
+        _simplePrimGraph.addVertex(start);
+        
+        insertNeighboursIntoHeap(start);
+        
+        while(!_fibHeap.isEmpty())
+        {
+            
+        }
+    }
+    
+    private void insertNeighboursIntoHeap(Vertex start)
+    {
+        Vertex source;
+        Vertex target;
+        Vertex child = null;
+        double kantenGewicht;
+        for(MyWeightedEdge edge : _eingabeGraph.edgesOf(start))
+        {
+            _graphAccesses++;
+            source = _eingabeGraph.getEdgeSource(edge);
+            target = _eingabeGraph.getEdgeTarget(edge);
+            child = null;
+            kantenGewicht = edge.getEdgeWeight();
+            
+            if(source.equals(start))
+            {
+                child = target;
+            }
+            else if(target.equals(start))
+            {
+                child = source;
+            }
+            
+            if(_simplePrimGraph.containsVertex(child)) continue;
+            if(!_fibInhalt.containsKey(child))// && kantenGewicht < _schlüssel.get(child))
+            {
+                _fibInhalt.put(child,new FibonacciHeapNode<Vertex>(child));
+                _schlüssel.put(child, kantenGewicht);
+                _fibHeap.insert(_fibInhalt.get(child), kantenGewicht);                
+            }
+            else if(_schlüssel.get(child) > kantenGewicht)
+            {
+                _fibHeap.decreaseKey(_fibInhalt.get(child), kantenGewicht);
+                _schlüssel.put(child, kantenGewicht);
+            }            
+        }
     }
 
+//    private void startAlgorithm()
+//    {
+//        startTime = System.nanoTime();
+//        System.out.println(startTime);
+//        Map<Vertex,Vertex> vorgaenger = new HashMap<>();
+//        for(Vertex v : _simplePrimGraphVertices)
+//        {
+//            _graphAccesses++;
+//            _schlüssel.put(v, Double.POSITIVE_INFINITY);
+//            vorgaenger.put(v, null);
+//            _simplePrimGraph.addVertex(v);
+//        }
+//        
+//        Vertex tempVertex = _simplePrimGraphVertices.get(0);      
+//        _schlüssel.put(tempVertex, 0.0); // schlüssel[r] = 0
+//        _fibHeap.insert(new FibonacciHeapNode<Vertex>(tempVertex), 0.0);
+//        double kantenGewicht;
+//        MyWeightedEdge edge = null;
+//        Vertex child= null;
+//        
+//        while(!_fibHeap.isEmpty())
+//        {   
+//            _graphAccesses++;
+//            tempVertex = _fibHeap.removeMin().getData();         
+//            
+//            for(MyWeightedEdge e : _eingabeGraph.edgesOf(tempVertex))
+//            {
+//                _graphAccesses++;
+//                kantenGewicht = e.getEdgeWeight();
+//                if(_eingabeGraph.getEdgeSource(e).equals(tempVertex))
+//                {
+//                    child = _eingabeGraph.getEdgeTarget(e);
+//                } else{
+//                    child = _eingabeGraph.getEdgeSource(e);
+//                }
+//                if(_fibHeap.)//_prioQueue.contains(child) && kantenGewicht < _schlüssel.get(child))
+//                {
+//                    vorgaenger.put(child, tempVertex);
+//                    _schlüssel.put(child, kantenGewicht);
+//                    
+//                }
+//                else if(vorgaenger.get(child) == null){
+//                    vorgaenger.put(child, tempVertex);
+//                    _schlüssel.put(child, kantenGewicht);
+//                    _prioQueue.add(child);
+//                }
+//            }            
+//        }
+//
+//        endTime = System.nanoTime();
+//        System.out.println(endTime);
+//        for(Vertex v : _simplePrimGraphVertices)
+//        {
+//            _graphAccesses++;
+//            if(vorgaenger.get(v) != null)
+//            {
+//                _kantenAnzahl++;
+//                edge = _eingabeGraph.getEdge(v, vorgaenger.get(v));
+//                _simplePrimGraph.addEdge(v, vorgaenger.get(v), edge);
+//            }           
+//        }
+//        _eingabeGraph = _simplePrimGraph;
+//    }
+
+//
+    private Set<Vertex> getUndirectedAdjacentNodes(Vertex n)
+    {
+        Set<Vertex> adjacentNodes= new HashSet<Vertex>();
+        Set<MyWeightedEdge> edges= _eingabeGraph.edgesOf(n);
+       for(MyWeightedEdge edge : edges)
+       {
+           Vertex source = _eingabeGraph.getEdgeSource(edge);
+           Vertex neighbour = _eingabeGraph.getEdgeTarget(edge);
+           if(source.equals(n))
+           {
+               adjacentNodes.add(neighbour);               
+           } else if(neighbour.equals(n))
+           {
+               adjacentNodes.add(source);
+           }
+       }
+       return adjacentNodes;        
+    }
+    
     public Graph<Vertex, MyWeightedEdge> getGraph()
     {
-        return _graph;
+        return _simplePrimGraph;
+    }
+    
+    public long getTime()
+    {
+        return endTime-startTime;
+    }
+    
+    public double getWeglaenge()
+    {
+        double laenge = 0;
+        for(Vertex v : _schlüssel.keySet())
+        {
+            laenge += _schlüssel.get(v);
+        }
+        return laenge;
     }
 
+    public int getAnzahlBenoetigteKanten()
+    {
+        return _kantenAnzahl-1;
+    }
+
+    public int getAccesses()
+    {
+        return _graphAccesses;
+    }
+
+    
 }
